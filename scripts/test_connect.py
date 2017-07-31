f06ee7081be8e1e2c19f7b890f8bdfe11178cffa
lrodriguez15 <leopoldo.rodriguez0303@gmail.com>
2017-07-31 15:08:46 +0200

    Test Connection

--- scripts/test_connect.py
+++ scripts/test_connect.py
@@ -4,27 +4,26 @@ import time
 class UAS(object):
     def __init__(self):
         self.vehicle = self.get_vehicle()
-
-
         self.initialize = False
 
     def get_vehicle(self):
         print "connecting to vehicle.."
-        return connect('/dev/ttyUSB0', wait_ready=True, baud=57600)
+        return connect('/dev/ttyUSB0', baud=57600)
 
     def initialize_vehicle(self):
         if self.vehicle is None:
             self.vehicle = self.get_vehicle()
-            print "vehicle connected.."
             return True;
 
     def run(self):
         while True:
             if self.initialize is False:
                 self.initialize = self.initialize_vehicle()
-            else:
-                print "Vehicle OK"
-
+                print "Autopilot Firmware version: %s" % self.vehicle.version
+                print "Is Armable?: %s" % self.vehicle.is_armable
+                print "System status: %s" % self.vehicle.system_status.state
+                print "Mode: %s" % self.vehicle.mode.name  # settable
+                print "Armed: %s" % self.vehicle.armed  # settable
             time.sleep(0.05)
 
 uas = UAS()
